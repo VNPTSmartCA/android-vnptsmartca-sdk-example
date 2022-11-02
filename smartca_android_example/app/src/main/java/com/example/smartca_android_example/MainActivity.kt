f593420c35fd9b3a96c821fe03e49a15b3a831aa
VNPTSmartCA <smartca.vnptit@gmail.com>
2022-11-02 09:04:41 +0700

    update example

--- smartca_android_example/app/src/main/java/com/example/smartca_android_example/MainActivity.kt
+++ smartca_android_example/app/src/main/java/com/example/smartca_android_example/MainActivity.kt
@@ -6,10 +6,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
-import com.onetimeca.vnpt_smartca.ConfigSDK
-import com.onetimeca.vnpt_smartca.OnetimeVNPTSmartCA
-import com.onetimeca.vnpt_smartca.SmartCAEnvironment
-import com.onetimeca.vnpt_smartca.SmartCAResultCode
+import com.onetimeca.vnpt_smartca.*
+
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.lang.Exception
@@ -21,22 +19,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        val btn_click_me = findViewById(R.id.button) as Button
-        val edit_text_trans = findViewById(R.id.plain_text_input) as EditText
-        val btn_create_trans = findViewById(R.id.button1) as Button
-
+        val btn_click_me = findViewById<Button>(R.id.button)
+        val edit_text_trans = findViewById<EditText>(R.id.plain_text_input)
+        val btn_create_trans = findViewById<Button>(R.id.button1)
+        val btnHomePage = findViewById<Button>(R.id.btnHomePage)
 
         val config = ConfigSDK()
         config.context = this
         //Cấu hình partnerId, partnerId của đối tác được VNPTSmartCA cung cấp khi yêu cầu tích hợp.
-        config.partnerId = "xxx-xxx-xxx-xxx"
+        config.partnerId = "VNPTSmartCAPartner-add1fb94-9629-49`47-b7d8-f2671b04c747"
         //Cấu hình môi trường Dev-test hay Production cùa SmartCA
         config.environment = SmartCAEnvironment.DEMO_ENV
         //Cấu hình ngôn ngữ app (vi/en)
-        config.lang = "vi"
-
-
-
+        config.lang = SmartCALanguage.VI
+        println( config.lang)
 
         onetimeVNPTSmartCA.initSDK(config)
 
@@ -49,6 +45,10 @@ class MainActivity : AppCompatActivity() {
         btn_create_trans.setOnClickListener {
             val transId = edit_text_trans.text.toString()
             getWaitingTransaction(transId)
+        }
+
+        btnHomePage.setOnClickListener{
+            getMainInfo()
         }
     }
 
@@ -107,6 +107,29 @@ class MainActivity : AppCompatActivity() {
                         //
                     }
 
+                }
+            }
+        } catch (ex: Exception) {
+            throw  ex;
+        }
+    }
+
+    fun getMainInfo() {
+        try {
+            onetimeVNPTSmartCA.getAuthentication { result ->
+                when (result.status) {
+                    SmartCAResultCode.SUCCESS_CODE -> {
+                        onetimeVNPTSmartCA.getMainInfo { result ->
+                            when (result.status) {
+                                SmartCAResultCode.SUCCESS_CODE -> {
+
+                                }
+                                else -> {
+                                    // Error here
+                                }
+                            }
+                        }
+                    }
                 }
             }
         } catch (ex: Exception) {
